1083f82d52d406fda2315a604ea0b15135061e8b
ebrahim-sani <ibraheemsani035@gmail.com>
2022-08-27 17:39:05 +0100

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -15,9 +15,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Provider store={store}>
             <Router>
-                <Routes>
-                    <Route path="/*" element={<App />} />
-                </Routes>
+                <Toaster />
+                <App />
             </Router>
         </Provider>
     </React.StrictMode>,
